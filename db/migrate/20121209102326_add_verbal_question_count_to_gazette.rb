44297310107350591e3c05ceba1b0802eb1db596
xdite <xdite@rocodev.com>
2012-12-09 18:26:24 +0800

    fix typo

--- db/migrate/20121209102326_add_verbal_question_count_to_gazette.rb
+++ db/migrate/20121209102326_add_verbal_question_count_to_gazette.rb
@@ -1,5 +1,5 @@
 class AddVerbalQuestionCountToGazette < ActiveRecord::Migration
   def change
-    add_column :gazettes, :verbal_questions_count, :default => 0
+    add_column :gazettes, :verbal_questions_count, :integer, :default => 0
   end
 end
