392c3b46b8d206287e4ac7a4256eed16afdf3b3f
xdite <xdite@rocodev.com>
2012-12-09 13:20:25 +0800

    search

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,3 +1,17 @@
 # -*- encoding : utf-8 -*-
 module SearchHelper
+
+ def search_match_result(text,query_string)
+    tokens = query_string.split(/[\\. ,+#-%^&*]/)
+    text = text.gsub(/<.*?>/,"")
+    return_string ||= ""
+    tokens.each do |token|
+      return_string.concat(%Q|#{h(excerpt(text, token, :radius => 100)) }|)
+    end
+
+    return return_string
+
+  end
+
+
 end
